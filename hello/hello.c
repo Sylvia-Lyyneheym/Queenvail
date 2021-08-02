6e2f9ac39f3c127650bd60f95eab7ba8979ad088
sylvia <847759293@qq.com>
2021-08-02 16:10:23 +0800

    logs

---
+++ hello/hello.c
@@ -0,0 +1,21 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+static int hello_init(void)
+{
+	printk(KERN_INFO "Hello Word\n");
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk(KERN_INFO "Good Bye\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("TSBSP");
+MODULE_DESCRIPTION("Hello World Module");
